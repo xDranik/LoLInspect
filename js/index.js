455d6e1b9f6b99960e1a15c7b5a8a017567deb6e
Andranik Andy Tonoyan <xDranik@gmail.com>
2013-12-14 15:51:01 -0800

    Created query summoner forms

--- js/index.js
+++ js/index.js
@@ -10,6 +10,11 @@ lolApp.controller('LeftSummonerCtrl', function($scope){
       'Draven',
       'Thresh'
    ];
+
+   $scope.queryUser = function(){
+      console.log('test queryUser in LeftSummonerCtrl');
+      //validate user input?
+   }
 });
 
 lolApp.controller('RightSummonerCtrl', function($scope){
@@ -22,8 +27,32 @@ lolApp.controller('RightSummonerCtrl', function($scope){
       'Draven',
       'Thresh'
    ];
+
+   $scope.queryUser = function(){
+      console.log('test queryUser in RightSummonerCtrl');
+      //validate user input?
+   }
 });
 
 lolApp.controller('StatComparisonCtrl', function($scope){
    //todo
 });
+
+lolApp.directive('summonerForm', function(){
+   return {
+      restrict: 'E',
+      template:'<form role="form" ng-submit=queryUser()>'+
+                  '<div class="col-xs-10">'+
+                     '<input type="text" class="form-control margin-top-10" placeholder="Summoner Name">'+
+                  '</div>'+
+                  '<div class="col-xs-3 margin-top-10">'+
+                     '<select class="form-control">'+
+                        '<option value="na">NA</option>'+ 
+                        '<option value="eu">EU</option>'+
+                        '<option value="eune">EUNE</option>'+
+                     '</select>'+
+                  '</div>'+
+                  '<button type="submit" class="btn btn-primary margin-top-10">Search Summoner</button>'+
+               '</form>'
+   }
+})
