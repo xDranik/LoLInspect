5abd2469e033a08d3790458adcbcb454c645a4a0
Andranik Andy Tonoyan <xDranik@gmail.com>
2013-12-31 02:17:08 -0800

    Selecting champions for comparison

--- js/index.js
+++ js/index.js
@@ -1,18 +1,96 @@
 var lolApp = angular.module('lolApp', []);
 
-lolApp.controller('LeftSummonerCtrl', function($scope, $http){
+lolApp.factory('StatCompareService', function(){
+
+   return {
+      leftChampionData: {name: '?'},
+      rightChampionData: {name: '?'},
+      comparisonData: undefined
+   }
+
+});
+
+lolApp.controller('LeftSummonerCtrl', function($scope, $http, StatCompareService){
 
    $scope.regions = ['NA', 'EUW', 'EUNE'];
    $scope.region = $scope.regions[0];
    $scope.showSummoner = false;
    $scope.apikey = '???';
 
+   $scope.champions = [];
+
+
    $scope.querySummoner = function(){
-
       getSummonerIDFromName($scope, $http);
-
    }
+
+   $scope.updateStatCompareData = function(champion){
+      StatCompareService.leftChampionData = champion;
+      console.log(StatCompareService);
+   }
+
+   $scope.test = StatCompareService.leftChampionData.name;
+
 });
+
+
+lolApp.controller('RightSummonerCtrl', function($scope, $http, StatCompareService){
+
+   $scope.regions = ['NA', 'EUW', 'EUNE'];
+   $scope.region = $scope.regions[0];
+   $scope.showSummoner = false;
+   $scope.apikey = '???';
+
+   $scope.champions = [];
+
+   $scope.querySummoner = function(){
+      getSummonerIDFromName($scope, $http);
+   }
+
+
+   $scope.updateStatCompareData = function(champion){
+      StatCompareService.rightChampionData = champion;
+      console.log(StatCompareService);
+   }
+
+});
+
+lolApp.controller('ComparisonCtrl', function($scope, StatCompareService){
+
+   $scope.leftChampionName = StatCompareService;
+   $scope.rightChampionName = StatCompareService;
+
+});
+
+lolApp.directive('summonerForm', function(){
+   return {
+      restrict: 'E',
+      template:'<form role="form" ng-submit=querySummoner()>'+
+                  '<div class="col-xs-10">'+
+                     '<input type="text" class="form-control margin-top-10" placeholder="Summoner Name" ng-model="summonerName">'+
+                  '</div>'+
+                  '<div class="col-xs-4 margin-top-10">'+
+                     '<select class="form-control" ng-model="region" ng-options="r.toString() for r in regions">'+
+                     '</select>'+
+                  '</div>'+
+                  '<button type="submit" class="btn btn-primary margin-top-10">Search Summoner</button>'+
+               '</form>'
+   }
+})
+
+lolApp.directive('championList', function(){
+   return {
+      restrict: 'E',
+      template:'<div ng-show="showSummoner">'+
+                  '<hr>Select a Champion:'+
+                  '<div ng-repeat="champion in champions|orderBy:\'name\'">'+
+                     '<h4 ng-click="updateStatCompareData(champion)">{{champion.name}}</h4>'+//don't need {{}}???
+                  '</div>'+
+               '</div>'
+   }
+})
+
+
 
 
 function getSummonerIDFromName($scope, $http){
@@ -32,7 +110,6 @@ function getSummonerIDFromName($scope, $http){
    })
    .success(function(data, status, headers, config){
 
-      $scope.showSummoner = true;
       console.log(data);
       getSummonerRankedStats($scope, $http, data.id)
 
@@ -56,9 +133,12 @@ function getSummonerRankedStats($scope, $http, summonerID){
       summonerID+'/ranked?season=SEASON3&api_key='+$scope.apikey
    })
    .success(function(data, status, headers, config){
+      $scope.champions = data.champions;
+      $scope.showSummoner = true;
       console.log(data);
    })
    .error(function(data, status, headers, config){
+      $scope.showSummoner = false;
       console.log('faku in get ranked stats');
    });
 
@@ -67,36 +147,3 @@ function getSummonerRankedStats($scope, $http, summonerID){
 function handleStatResponse($scope){
 
 }
-
-
-
-lolApp.controller('RightSummonerCtrl', function($scope){
-
-   $scope.regions = ['NA', 'EU', 'EUNE'];
-   $scope.region = $scope.regions[0];
-
-   $scope.queryUser = function(){
-      console.log('test queryUser in RightSummonerCtrl');
-      //validate user input?
-   }
-});
-
-lolApp.controller('ComparisonCtrl', function($scope){
-   //todo
-});
-
-lolApp.directive('summonerForm', function(){
-   return {
-      restrict: 'E',
-      template:'<form role="form" ng-submit=querySummoner()>'+
-                  '<div class="col-xs-10">'+
-                     '<input type="text" class="form-control margin-top-10" placeholder="Summoner Name" ng-model="summonerName">'+
-                  '</div>'+
-                  '<div class="col-xs-4 margin-top-10">'+
-                     '<select class="form-control" ng-model="region" ng-options="r.toString() for r in regions">'+
-                     '</select>'+
-                  '</div>'+
-                  '<button type="submit" class="btn btn-primary margin-top-10">Search Summoner</button>'+
-               '</form>'
-   }
-})
