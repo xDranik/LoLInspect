0c86a0f865ab44df3ed253e47c7ec5fd557b9bbf
Andranik Andy Tonoyan <xDranik@gmail.com>
2014-01-03 13:32:28 -0800

    Switched from directive template to directive templateUrl

--- js/directives.js
+++ js/directives.js
@@ -3,35 +3,13 @@ var app = angular.module('app.directives', []);
 app.directive('summonerForm', function(){
    return {
       restrict: 'E',
-      template:'<form role="form" ng-submit=querySummoner()>'+
-                  '<div class="col-xs-12">'+
-                     '<h3>Summoner Search</h3>'+
-                     '<input type="text" class="form-control" placeholder="Summoner Name" ng-model="summonerName">'+
-                  '</div>'+
-                  '<div class="col-xs-5 margin-top-10">'+
-                     '<select class="form-control" ng-model="region" ng-options="r.toString() for r in regions">'+
-                     '</select>'+
-                  '</div>'+
-                  '<button type="submit" class="btn btn-primary margin-top-10">'+
-                     '<span class="glyphicon glyphicon-search"></span> Search'+
-                  '</button>'+
-               '</form>'
+      templateUrl: '../html/partials/directives/summoner_form.html'
    }
 });
 
 app.directive('championList', function(){
    return {
       restrict: 'E',
-      template:'<div class="col-xs-12" ng-show="showSummoner">'+
-                  '<hr><p>Select a Champion:</p>'+
-                  '<input type="text" class="form-control" placeholder="Champion search" '+
-                     'ng-model="championFilter" ng-focus="showChampionList=true;">'+
-                  '<table class="table table-bordered" ng-show="showChampionList">'+
-                     '<tr ng-repeat="champion in champions|filter:championFilter|orderBy:\'name\'|slice:0:10:championFilter" '+
-                        'ng-click="updateStatCompareData(champion)">'+
-                        '<td class="cursor-pointer">{{champion.name}}</td>'+
-                     '</tr>'+
-                  '</table>'+
-               '</div>'
+      templateUrl: '../html/partials/directives/champion_list.html'
    };
 });
