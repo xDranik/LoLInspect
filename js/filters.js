7a305017b75401b9f96d4dd27e174670f17ea761
Andranik Andy Tonoyan <xDranik@gmail.com>
2014-01-05 00:07:19 -0800

    Removed unnecessary filters

--- js/filters.js
+++ js/filters.js
@@ -1,9 +1,1 @@
 var app = angular.module('app.filters', [])
-
-//slice to unemptySlice
-app.filter('slice', function() {
-   return function(arr, start, end, championFilter) {
-      championFilter = championFilter || '';
-      return championFilter == '' ? [] : arr.slice(start, end);
-   };
-});
